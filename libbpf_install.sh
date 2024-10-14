8429c5ef68da63a2e189e307abc05e85e4a7be9a
Nima Fallah <nfallah399@gmail.com>
2024-10-14 12:39:41 -0400

    Renamed map_addition.txt

--- libbpf_install.sh
+++ libbpf_install.sh
@@ -24,7 +24,7 @@ git checkout $version
 cd src
 echo -e "\n#include \"libbpf_extractor.h\"" >> libbpf.c
 INSERT_LINE=$(($(wc -l < libbpf.map) - 1))
-sed "s/^/\t\t/; s/$/;/" ../../map_addition.txt > .tmp_1
+sed "s/^/\t\t/; s/$/;/" ../../map_addition > .tmp_1
 sed "${INSERT_LINE}r .tmp_1" libbpf.map > .tmp_2
 mv .tmp_2 libbpf.map
 rm .tmp_1
